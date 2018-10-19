813b01f72cab9eb8b566e97f0afa3674c5f6677e
Arya Khaligh <a.khaligh@aut.ac.ir>
2018-10-19 17:44:06 +0330

    Empty string({}) bug fixed !

--- DFA_acceptor.py
+++ DFA_acceptor.py
@@ -6,7 +6,10 @@ def split_transitions(transition):
             found = True
         elif found:
             found = False
-            array.append(element)
+            if element == "}":
+                array.append("")
+            else :
+                array.append(element)
     return array
 
 
@@ -51,24 +54,45 @@ def get_dfa_elements(dfa_array):
     return main_dict
 
 
-def read_strings():
-    dfa_strings = open("Strings_for_first_DFA.txt", "r").read().split("\n")
+def read_strings(string_file_name):
+    dfa_strings = open(string_file_name, "r").read().split("\n")
     return dfa_strings
 
 
 def check_state_is_final(state, dfa_dict):
     is_final = False
     for element in dfa_dict['F']:
-        if state == element :
+        if state == element:
             is_final = True
     return is_final
 
 
-dfa_dict = get_dfa_elements(open("FIRST_DFA.txt", "r").read().split("\n"))
-print(dfa_dict)
-present_state = dfa_dict["I"][0]
-for element in read_strings()[0]:
-    transition_index = dfa_dict['E'].index(element)
-    present_state = dfa_dict['T'][present_state][transition_index]
-    print(present_state)
-print (check_state_is_final(present_state , dfa_dict) )
+def print_results(result_array):
+    print("RESULTS : ")
+    counter = 1
+    for element in result_array:
+        print(str(counter) + " => " + str(element))
+        counter += 1
+
+
+def main_func(machine_file_name, string_file_name):
+    dfa_dict = get_dfa_elements(open(machine_file_name, "r").read().split("\n"))
+    print(dfa_dict)
+    result_array = []
+    for string in read_strings(string_file_name):
+        present_state = dfa_dict["I"][0]
+        for element in string:
+            transition_index = dfa_dict['E'].index(element)
+            present_state = dfa_dict['T'][present_state][transition_index]
+        result_array.append(check_state_is_final(present_state, dfa_dict))
+    print_results(result_array)
+
+
+FILES_NAME = (("FIRST_DFA.txt", "Strings_for_first_DFA.txt"),
+              ("Second_DFA.txt", "Strings_for_second_DFA.txt"),
+              ("Third_DFA.txt", "Strings_for_third_DFA.txt"),
+              ("Fourth_DFA.txt", "Strings_for_fourth_DFA.txt"),
+              ("Fifth_DFA.txt", "Strings_for_fifth_DFA.txt"))
+
+for item in FILES_NAME:
+    main_func(item[0], item[1])
